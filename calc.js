bdf732014b6475c8eb91578c9f41667ef3d0f561
Júlio César Kraemer <jcesarkraemer@gmail.com>
2022-05-21 18:17:55 -0300

    commit parte final da calc.js

--- calc.js
+++ calc.js
@@ -7,3 +7,13 @@ function inserir(num){
 function limpar(){
     document.getElementById("resultado").innerHTML = "";
 }
+function apagar(){
+    let resultado = document.getElementById("resultado").innerHTML;
+    document.getElementById("resultado").innerHTML = resultado.substring(0, resultado.length -1);
+}
+function calcular(){
+    let resultado = document.getElementById("resultado").innerHTML;
+    if(resutado){
+        document.getElementById("resultado").innerHTML = eval(resultado);
+    }
+}
